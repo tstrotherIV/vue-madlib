0e50284e17626cf212b9f9a36ac7dbe2c02572d0
Tom Strother <tom_strother@yahoo.com>
2021-03-25 07:53:49 -0500

    m

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,48 +1,71 @@
 <template>
   <div>
     <v-container>
-      <h1>Create Madlib</h1>
-      <v-btn @click="getTemplate">Get template</v-btn>
+      <v-btn @click="addToFirestore">Save To Firestore</v-btn>
+      <div>
+        <h1 v-if="!this.madlib">Create Madlib</h1>
+        <h1 v-else>Create New Madlib</h1>
+      </div>
+      <div>
+        <v-btn v-if="!this.madlib" @click="getTemplate">Get template</v-btn>
+        <v-btn v-else @click="getTemplate">Get New template</v-btn>
+      </div>
     </v-container>
-    <v-container v-if="this.prompts">
-      <v-form @submit.prevent="saveInputs">
-        <v-text-field
-          v-for="(blanks, index) in prompts.blanks"
-          :key="index"
-          :placeholder="blanks"
-          v-model="prompts.inputs[index]"
-          outlined
-        ></v-text-field>
-        <v-btn type="submit">Save Answers</v-btn>
-      </v-form>
+    <div v-if="!this.madlib">
+      <v-container v-if="this.prompts">
+        <v-form @submit.prevent="putMadlibTogether">
+          <v-text-field
+            v-for="(blanks, index) in prompts.blanks"
+            :key="index"
+            :placeholder="blanks"
+            v-model="prompts.inputs[index]"
+            outlined
+          ></v-text-field>
+          <v-btn v-if="prompts.blanks" type="submit">Create Madlib</v-btn>
+        </v-form>
+      </v-container>
+    </div>
+    <v-container v-if="madlib">
+      <p>
+        {{ madlib }}
+      </p>
     </v-container>
   </div>
 </template>
 
 <script>
 import { db } from "../firebase";
-
 export default {
   methods: {
     async getTemplate() {
       this.prompts = [];
-      fetch("http://madlibz.herokuapp.com/api/random")
+      this.madlib = null;
+      fetch("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=10")
         .then((res) => res.json())
         .then((data) => {
           this.prompts = data;
           this.prompts.inputs = [];
         });
     },
-    async saveInputs() {
-      await db.collection("userInputs").add({
+    async putMadlibTogether() {
+      const madlib = await this.prompts.value.map((e, i) => {
+        return e + this.prompts.inputs[i];
+      });
+      this.madlib = await madlib.join(" ");
+    },
+    async addToFirestore() {
+      console.log("ready to send");
+      await db.collection("firstOne").add({
         data1: "test1",
         data2: "test2",
       });
+      console.log("ran");
     },
   },
   data() {
     return {
       prompts: null,
+      madlib: null,
     };
   },
 };
